01299ea5adc308fc76aed148972dfe0578c6fdbe
Lander Brandt <landerbrandt@gmail.com>
2020-12-18 16:50:07 -0800

    Add display formatter for instructions

--- src/opcode/mod.rs
+++ src/opcode/mod.rs
@@ -8,6 +8,18 @@ pub struct Instruction<O: Opcode> {
     pub arg: Option<u16>,
 }
 
+use std::fmt::{self, Debug};
+impl<O: Opcode + Debug> fmt::Display for Instruction<O> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?}", self.opcode)?;
+        if let Some(arg) = self.arg {
+            write!(f, " {}", arg)?;
+        }
+
+        Ok(())
+    }
+}
+
 /// Trait that provides convenience routines for opcode properties such as whether
 /// or not it has an argument, is a jump, etc.
 pub trait Opcode {
